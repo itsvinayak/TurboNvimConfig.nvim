14e85512529d0b2d1feec1d3d4e810cffe82fafd
vinayak <vinayak@pratishthanventures.com>
2024-03-12 15:49:20 +0530

    bug: change keymapping to <leader>of

--- init.lua
+++ init.lua
@@ -5,7 +5,7 @@ require("NeoMinimalVim.plugins.init")
 vim.cmd([[command! CheckHealth lua require('main.health').check()]])
 
 vim.o.background = "dark" -- or "light" for light mode
-vim.cmd([[colorscheme gruvbox]])
 
 
 require("notify")("configuration loaded . . .", "info")
+vim.cmd("colorscheme gruvbox")

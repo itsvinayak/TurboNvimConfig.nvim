bb20fb1afb3d0a0524d415494cf4271f37fa07c5
vinayak <itssvinayak@gmail.com>
2024-03-14 23:35:57 +0530

    fix: dashboard

--- lua/NeoMinimalVim/plugins/dashboard.lua
+++ lua/NeoMinimalVim/plugins/dashboard.lua
@@ -8,82 +8,89 @@ local header = [[
 ]]
 
 return {
-	{
-		"nvimdev/dashboard-nvim",
-		dependencies = { { "nvim-tree/nvim-web-devicons" } },
-		event = "VimEnter",
-		opts = function()
-			local logo = header
+  {
+    'nvimdev/dashboard-nvim',
+    dependencies = { { 'nvim-tree/nvim-web-devicons' } },
+    event = 'VimEnter',
+    opts = function()
+      local logo = header
 
-			logo = string.rep("\n", 8) .. logo
+      logo = string.rep('\n', 8) .. logo
 
-			local opts = {
-				theme = "doom",
-				hide = {
-					-- this is taken care of by lualine
-					-- enabling this messes up the actual laststatus setting after loading a file
-					statusline = false,
-				},
-				config = {
-					header = vim.split(logo, "\n"),
-                center = {{
-                    action = "Telescope find_files",
-                    desc = " Find file",
-                    icon = "󰮗",
-                    key = "f"
-                }, {
-                    action = "ene | startinsert",
-                    desc = " New file",
-                    icon = " ",
-                    key = "n"
-                }, {
-                    action = "Telescope oldfiles",
-                    desc = " Recent files",
-                    icon = " ",
-                    key = "r"
-                }, {
-                    action = "Telescope live_grep",
-                    desc = " Find text",
-                    icon = "󰗧",
-                    key = "g"
-                }, {
-                    action = "Lazy",
-                    desc = " Lazy",
-                    icon = "󰒲 ",
-                    key = "l"
-                }, {
-                    action = "qa",
-                    desc = " Quit",
-                    icon = " ",
-                    key = "q"
-                }},
-					footer = function()
-						local stats = require("lazy").stats()
-						local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
-						return {
-							"⚡ Neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms",
-						}
-					end,
-				},
-			}
+      local opts = {
+        theme = 'doom',
+        hide = {
+          -- this is taken care of by lualine
+          -- enabling this messes up the actual laststatus setting after loading a file
+          statusline = false,
+        },
+        config = {
+          header = vim.split(logo, '\n'),
+          center = {
+            {
+              action = 'Telescope find_files',
+              desc = ' Find file',
+              icon = '󰮗',
+              key = 'f',
+            },
+            {
+              action = 'ene | startinsert',
+              desc = ' New file',
+              icon = ' ',
+              key = 'n',
+            },
+            {
+              action = 'Telescope oldfiles',
+              desc = ' Recent files',
+              icon = ' ',
+              key = 'r',
+            },
+            {
+              action = 'Telescope live_grep',
+              desc = ' Find text',
+              icon = '󰗧',
+              key = 'g',
+            },
+            {
+              action = 'Lazy',
+              desc = ' Lazy',
+              icon = '󰒲 ',
+              key = 'l',
+            },
+            {
+              action = 'qa',
+              desc = ' Quit',
+              icon = ' ',
+              key = 'q',
+            },
+          },
+          footer = function()
+            local stats = require('lazy').stats()
+            local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+            return {
+              '⚡ Neovim loaded ' .. stats.loaded .. '/' .. stats.count .. ' plugins in ' .. ms .. 'ms',
+            }
+          end,
+        },
+      }
 
-			for _, button in ipairs(opts.config.center) do
-				button.desc = button.desc .. string.rep(" ", 43 - #button.desc)
-				button.key_format = "  %s"
-			end
+      for _, button in ipairs(opts.config.center) do
+        button.desc = button.desc .. string.rep(' ', 43 - #button.desc)
+        button.key_format = '  %s'
+      end
 
-			-- close Lazy and re-open when the dashboard is ready
-			if vim.o.filetype == "lazy" then
-				vim.cmd.close()
-				vim.api.nvim_create_autocmd("User", {
-					pattern = "DashboardLoaded",
-					callback = function()
-						require("lazy").show()
-					end,
-				})
-			end
+      -- close Lazy and re-open when the dashboard is ready
+      if vim.o.filetype == 'lazy' then
+        vim.cmd.close()
+        vim.api.nvim_create_autocmd('User', {
+          pattern = 'DashboardLoaded',
+          callback = function()
+            require('lazy').show()
+          end,
+        })
+      end
 
-			return opts
-		end,
-	},
+      return opts
+    end,
+  },
 }
--- lua/NeoMinimalVim/plugins/init.lua
+++ lua/NeoMinimalVim/plugins/init.lua
@@ -1,64 +1,59 @@
-if vim.fn.has("nvim-0.9.0") == 0 then
-	vim.api.nvim_echo(
-		{ { "LazyVim requires Neovim >= 0.9.0\n", "ErrorMsg" }, { "Press any key to exit", "MoreMsg" } },
-		true,
-		{}
-	)
-	vim.fn.getchar()
-	vim.cmd([[quit]])
-	return {}
+if vim.fn.has 'nvim-0.9.0' == 0 then
+  vim.api.nvim_echo({ { 'LazyVim requires Neovim >= 0.9.0\n', 'ErrorMsg' }, { 'Press any key to exit', 'MoreMsg' } }, true, {})
+  vim.fn.getchar()
+  vim.cmd [[quit]]
+  return {}
 end
 
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+  vim.fn.system {
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
+    lazypath,
+  }
 end
 
 vim.opt.rtp:prepend(lazypath)
 
 -- Plugins
 -- ============================================================================
-require("lazy").setup({
-	require("NeoMinimalVim.plugins.lualine"),
-	require("NeoMinimalVim.plugins.indent-blankline"),
-	require("NeoMinimalVim.plugins.debug"),
-	require("NeoMinimalVim.plugins.dashboard"),
-	'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
-	{"github/copilot.vim"}, -- Copilot AI
-	{"rcarriga/nvim-notify"}, -- Notifications
-	{
-        "kevinhwang91/nvim-ufo",
-        dependencies = "kevinhwang91/promise-async"
-    },
-	require("NeoMinimalVim.plugins.comment"),
-	require("NeoMinimalVim.plugins.neo-tree"),
-	{"wakatime/vim-wakatime"},
-	require("NeoMinimalVim.plugins.which-key"),
-	require("NeoMinimalVim.plugins.undotree"),
-	require("NeoMinimalVim.plugins.zen-mode"),
-        require("NeoMinimalVim.plugins.gruvbox"),
-	require("NeoMinimalVim.plugins.lsp"),
-	require("NeoMinimalVim.plugins.neoclip"),
-	require("NeoMinimalVim.plugins.telescope"),
-	require("NeoMinimalVim.plugins.git"),
-	require("NeoMinimalVim.plugins.conform"),
-})
+require('lazy').setup {
+  require 'NeoMinimalVim.plugins.lualine',
+  require 'NeoMinimalVim.plugins.debug',
+  require 'NeoMinimalVim.plugins.dashboard',
+  'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+  { 'github/copilot.vim' }, -- Copilot AI
+  { 'rcarriga/nvim-notify' }, -- Notifications
+  {
+    'kevinhwang91/nvim-ufo',
+    dependencies = 'kevinhwang91/promise-async',
+  },
+  require 'NeoMinimalVim.plugins.comment',
+  require 'NeoMinimalVim.plugins.neo-tree',
+  require 'NeoMinimalVim.plugins.indent-blankline',
+  { 'wakatime/vim-wakatime' },
+  require 'NeoMinimalVim.plugins.which-key',
+  require 'NeoMinimalVim.plugins.undotree',
+  require 'NeoMinimalVim.plugins.zen-mode',
+  require 'NeoMinimalVim.plugins.gruvbox',
+  require 'NeoMinimalVim.plugins.lsp',
+  require 'NeoMinimalVim.plugins.neoclip',
+  require 'NeoMinimalVim.plugins.telescope',
+  require 'NeoMinimalVim.plugins.git',
+  require 'NeoMinimalVim.plugins.conform',
+}
 
 -- ===============================================================================
 
-
 -- setup
-require("NeoMinimalVim.plugins.lspSetup")
-require("NeoMinimalVim.plugins.ufoSetup")
-require("NeoMinimalVim.plugins.whichKeySetup")
-require("NeoMinimalVim.plugins.indentBlanklineSetup")
-require("NeoMinimalVim.plugins.telescopeSetup")
-require("NeoMinimalVim.plugins.neoclipSetup")
-require("NeoMinimalVim.plugins.conformSetup")
+require 'NeoMinimalVim.plugins.lspSetup'
+require 'NeoMinimalVim.plugins.ufoSetup'
+require 'NeoMinimalVim.plugins.whichKeySetup'
+require 'NeoMinimalVim.plugins.indentBlanklineSetup'
+require 'NeoMinimalVim.plugins.telescopeSetup'
+require 'NeoMinimalVim.plugins.neoclipSetup'
+require 'NeoMinimalVim.plugins.conformSetup'

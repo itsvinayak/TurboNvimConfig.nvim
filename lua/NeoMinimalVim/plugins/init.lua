1f5066a2b599b3e38faf3d9a67140da9f505a774
vinayak <itssvinayak@gmail.com>
2024-03-16 16:48:37 +0530

    fix: version nvim

--- lua/NeoMinimalVim/plugins/init.lua
+++ lua/NeoMinimalVim/plugins/init.lua
@@ -1,4 +1,4 @@
-if vim.fn.has 'nvim1.9.0' == 0 then
+if vim.fn.has 'nvim' == 0 then
   vim.api.nvim_echo({ { 'LazyVim requires Neovim >= 0.9.0\n', 'ErrorMsg' }, { 'Press any key to exit', 'MoreMsg' } }, true, {})
   vim.fn.getchar()
   vim.cmd [[quit]]

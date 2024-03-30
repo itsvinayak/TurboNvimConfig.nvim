cd444daad6f3c86c183891e726ad30a985d5a466
vinayak <itssvinayak@gmail.com>
2024-03-30 23:06:25 +0530

    fix: dap working for nodejs

--- lua/NeoMinimalVim/plugins/debug.lua
+++ lua/NeoMinimalVim/plugins/debug.lua
@@ -1,17 +1,7 @@
 -- debug.lua
---
--- Shows how to use the DAP plugin to debug your code.
---
--- Primarily focused on configuring the debugger for Go, but can
--- be extended to other languages as well. That's why it's called
--- kickstart.nvim and not kitchen-sink.nvim ;)
-
-return {
-  -- NOTE: Yes, you can install new plugins here!
+local setup = {
   'mfussenegger/nvim-dap',
-  -- NOTE: And you can specify dependencies as well
   dependencies = {
-    -- Creates a beautiful debugger UI
     'rcarriga/nvim-dap-ui',
     'theHamsta/nvim-dap-virtual-text',
     -- nio https://github.com/nvim-neotest/nvim-nio
@@ -19,68 +9,93 @@ return {
     -- Installs the debug adapters for you
     'williamboman/mason.nvim',
     'jay-babu/mason-nvim-dap.nvim',
-
-    -- Add your own debuggers here
   },
-  config = function()
-    local dap = require 'dap'
-    local dapui = require 'dapui'
-
-    require('mason-nvim-dap').setup {
-      -- Makes a best effort to setup the various debuggers with
-      -- reasonable debug configurations
-      automatic_setup = true,
-
-      -- You can provide additional configuration to the handlers,
-      -- see mason-nvim-dap README for more information
-      handlers = {},
-
-      -- You'll need to check that you have the required things installed
-      -- online, please don't ask me how to install them :)
-      ensure_installed = {
-        -- Update this to ensure that you have the debuggers for the langs you want
-      },
-    }
-
-    -- Basic debugging keymaps, feel free to change to your liking!
-    vim.keymap.set('n', '<F5>', dap.continue, { desc = 'Debug: Start/Continue' })
-    vim.keymap.set('n', '<F1>', dap.step_into, { desc = 'Debug: Step Into' })
-    vim.keymap.set('n', '<F2>', dap.step_over, { desc = 'Debug: Step Over' })
-    vim.keymap.set('n', '<F3>', dap.step_out, { desc = 'Debug: Step Out' })
-    vim.keymap.set('n', '<leader>b', dap.toggle_breakpoint, { desc = 'Debug: Toggle Breakpoint' })
-    vim.keymap.set('n', '<leader>B', function()
-      dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
-    end, { desc = 'Debug: Set Breakpoint' })
-
-    -- Dap UI setup
-    -- For more information, see |:help nvim-dap-ui|
-    dapui.setup {
-      -- Set icons to characters that are more likely to work in every terminal.
-      --    Feel free to remove or use ones that you like more! :)
-      --    Don't feel like these are good choices.
-      icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
-      controls = {
-        icons = {
-          pause = '⏸',
-          play = '▶',
-          step_into = '⏎',
-          step_over = '⏭',
-          step_out = '⏮',
-          step_back = 'b',
-          run_last = '▶▶',
-          terminate = '⏹',
-          disconnect = '⏏',
-        },
-      },
-    }
-
-    -- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
-    vim.keymap.set('n', '<F7>', dapui.toggle, { desc = 'Debug: See last session result.' })
-
-    dap.listeners.after.event_initialized['dapui_config'] = dapui.open
-    dap.listeners.before.event_terminated['dapui_config'] = dapui.close
-    dap.listeners.before.event_exited['dapui_config'] = dapui.close
-
-    -- Install golang specific config
-  end,
 }
+
+function setup.config()
+  local dap = require 'dap'
+  local dapui = require 'dapui'
+
+  -- colors for virtual text
+  vim.api.nvim_set_hl(0, 'DapStoppedLine', { default = true, link = 'Visual' })
+  vim.api.nvim_set_hl(0, 'DapLogPointSign', { default = true, link = 'Comment' })
+  vim.api.nvim_set_hl(0, 'DapBreakpointSign', { default = true, link = 'Error' })
+
+  require('mason-nvim-dap').setup {
+    automatic_setup = true,
+    handlers = {
+      function(config)
+        require('mason-nvim-dap').default_setup(config)
+      end,
+      -- your dap setup
+      -- python
+      python = function(config)
+        config.adapters = {
+          type = 'executable',
+          command = '/usr/bin/python3',
+          args = {
+            '-m',
+            'debugpy.adapter',
+          },
+        }
+        require('mason-nvim-dap').default_setup(config) -- don't forget this!
+      end,
+      -- node2
+      node2 = function(config)
+        local js_mason_path = os.getenv 'HOME' .. '.local/share/nvim/mason/packages/node-debug2-adapter'
+        config.adapters = {
+          type = 'executable',
+          command = 'node-debug2-adapter',
+          args = {
+            '--node',
+            js_mason_path .. '/node_modules/vscode-node-debug2/out/src/nodeDebug.js',
+          },
+        }
+        require('mason-nvim-dap').default_setup(config) -- don't forget this!
+      end,
+    },
+    ensure_installed = {
+      'python',
+      'node2',
+      'chrome',
+      'js',
+    },
+  }
+
+  -- Basic debugging keymaps, feel free to change to your liking!
+  vim.keymap.set('n', 'dc', dap.continue, { desc = 'Debug: Start/Continue' })
+  vim.keymap.set('n', 'di', dap.step_into, { desc = 'Debug: Step Into' })
+  vim.keymap.set('n', 'ds', dap.step_over, { desc = 'Debug: Step Over' })
+  vim.keymap.set('n', 'dso', dap.step_out, { desc = 'Debug: Step Out' })
+  vim.keymap.set('n', '<leader>b', dap.toggle_breakpoint, { desc = 'Debug: Toggle Breakpoint' })
+  vim.keymap.set('n', '<leader>B', function()
+    dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
+  end, { desc = 'Debug: Set Breakpoint' })
+
+  -- Dap UI setup
+  -- For more information, see |:help nvim-dap-ui|
+  dapui.setup {
+    icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
+    controls = {
+      icons = {
+        pause = '⏸',
+        play = '▶',
+        step_into = '⏎',
+        step_over = '⏭',
+        step_out = '⏮',
+        step_back = 'b',
+        run_last = '▶▶',
+        terminate = '⏹',
+        disconnect = '⏏',
+      },
+    },
+  }
+
+  vim.keymap.set('n', 'dui', dapui.toggle, { desc = 'Debug: See last session result.' })
+
+  dap.listeners.after.event_initialized['dapui_config'] = dapui.open
+  dap.listeners.before.event_terminated['dapui_config'] = dapui.close
+  dap.listeners.before.event_exited['dapui_config'] = dapui.close
+end
+
+return setup
---
+++ test.js
@@ -0,0 +1,26 @@
+function sum(a, b) {
+  return a + b;
+}
+
+function sub(a, b) {
+  return a - b;
+}
+
+function mul(a, b) {
+  return a * b;
+}
+
+function div(a, b) {
+  return a / b;
+}
+
+function main() {
+  let a = 1;
+  let b = 2;
+  console.log(sum(a, b));
+  console.log(sub(a, b));
+  console.log(mul(a, b));
+  console.log(div(a, b));
+}
+
+main();

4237488dc2bd0fa7fc11e13465f5c9e0f4ff9505
vinayak <itssvinayak@gmail.com>
2024-04-28 19:03:06 +0530

    fix: barbar config added

--- lua/TurboNvimConfig/plugins/barbar.lua
+++ lua/TurboNvimConfig/plugins/barbar.lua
@@ -11,7 +11,7 @@ return {
     opts = function()
       require('barbar').setup {
         show_tab_indicators = true,
-        show_close_icon = false,
+        show_close_icon = true,
         show_buffer_close_icons = false,
         tab_size = 20,
         indicator_icon = '▎',
@@ -23,6 +23,52 @@ return {
         minimum_padding = 2,
         maximum_length = 20,
         enable_devicons = true,
+        icons = {
+          -- Configure the base icons on the bufferline.
+          -- Valid options to display the buffer index and -number are `true`, 'superscript' and 'subscript'
+          buffer_index = false,
+          buffer_number = false,
+          button = '',
+          -- Enables / disables diagnostic symbols
+          diagnostics = {
+            [vim.diagnostic.severity.ERROR] = { enabled = true, icon = 'ﬀ' },
+            [vim.diagnostic.severity.WARN] = { enabled = true },
+            [vim.diagnostic.severity.INFO] = { enabled = true },
+            [vim.diagnostic.severity.HINT] = { enabled = true },
+          },
+          gitsigns = {
+            added = { enabled = true, icon = '+' },
+            changed = { enabled = true, icon = '~' },
+            deleted = { enabled = true, icon = '-' },
+          },
+          filetype = {
+            -- Sets the icon's highlight group.
+            -- If false, will use nvim-web-devicons colors
+            custom_colors = false,
+
+            -- Requires `nvim-web-devicons` if `true`
+            enabled = true,
+          },
+          separator = { left = '▎', right = '' },
+
+          -- If true, add an additional separator at the end of the buffer list
+          separator_at_end = true,
+
+          -- Configure the icons on the bufferline when modified or pinned.
+          -- Supports all the base icon options.
+          modified = { button = '●' },
+          pinned = { button = '', filename = true },
+
+          -- Use a preconfigured buffer appearance— can be 'default', 'powerline', or 'slanted'
+          preset = 'default',
+
+          -- Configure the icons on the bufferline based on the visibility of a buffer.
+          -- Supports all the base icon options, plus `modified` and `pinned`.
+          alternate = { filetype = { enabled = false } },
+          current = { buffer_index = true },
+          inactive = { button = '×' },
+          visible = { modified = { buffer_number = false } },
+        },
         filter = function(bufnum)
           if vim.fn.bufname(bufnum) == '' then
             return false
